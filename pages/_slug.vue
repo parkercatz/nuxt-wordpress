4ac4151daefbf128a8b32a6b28418fa8670b46af
ParkerCatz <tksm.webdesign09@gmail.com>
2021-03-28 23:04:35 +0900

    add: _slugで各投稿を表示

---
+++ pages/_slug.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="post">
+        <h2>{{ post.title.rendered }}</h2>
+        <div v-html="post.content.rendered" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  async asyncData({ params }) {
+    return await axios
+      .get('http://nuxtwordpress.local/wp-json/wp/v2/posts/' + params.id)
+      .then((res) => {
+        return {
+          post: res.data,
+        }
+      })
+  },
+  data() {
+    return {
+      post: {},
+    }
+  },
+}
+</script>
